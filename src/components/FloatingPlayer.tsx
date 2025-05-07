1659866010f86c572cd4972ac9de81eec69c1029
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2025-05-07 15:11:57 +0530

    floating player component added

---
+++ src/components/FloatingPlayer.tsx
@@ -0,0 +1,33 @@
+import { Text, View, Image, Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { useAudioPlayerStatus } from "expo-audio";
+import { usePlayer } from "@/providers/PlayerProvider";
+import dummyBooks from "@/dummyBooks";
+
+export default function FloatingPlayer() {
+  const book = dummyBooks[0]; // Replace with your actual book data;
+  //const { player, book } = usePlayer();
+  //const playerStatus = useAudioPlayerStatus(player);
+
+  // console.log(JSON.stringify(playerStatus, null, 2));
+
+  if (!book) return null;
+
+  return (
+    <Link href="/player" asChild>
+      <Pressable className="flex-row gap-4 items-center p-2 bg-slate-900">
+        <Image
+          source={{ uri: book.thumbnail_url }}
+          className="w-16 aspect-square rounded-md"
+        />
+        <View className="gap-1 flex-1">
+          <Text className="text-2xl text-gray-100 font-bold">{book.title}</Text>
+          <Text className="text-gray-400">{book.author}</Text>
+        </View>
+
+        <AntDesign name={"playcircleo"} size={24} color="gainsboro" />
+      </Pressable>
+    </Link>
+  );
+}
